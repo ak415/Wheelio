b7e9936b56598d3172a3f795789a2d33c973e31e
Akram <azaiezakrem@gmail.com>
2018-01-30 11:57:42 -0800

    initialized npm and tested rendering of root file. Status: Ok

---
+++ app/controllers/static_page_controller.rb
@@ -0,0 +1,5 @@
+class StaticPageController < ApplicationController
+  def root
+    render :root
+  end
+end
---
+++ test/controllers/static_page_controller_test.rb
@@ -0,0 +1,9 @@
+require 'test_helper'
+
+class StaticPageControllerTest < ActionDispatch::IntegrationTest
+  test "should get root" do
+    get static_page_root_url
+    assert_response :success
+  end
+
+end
