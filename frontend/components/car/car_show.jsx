445611c63f344dc12c9c73d245239a475d9ae771
Akram <azaiezakrem@gmail.com>
2018-02-03 14:51:57 -0800

    formatting car pages. started flexing price, mpg and 0-60

--- frontend/components/car/car_show.jsx
+++ frontend/components/car/car_show.jsx
@@ -25,9 +25,21 @@ class CarShow extends React.Component {
     if (this.state.images){
       img = this.state.images[0].image_url;
     }
+    let price= "$";
+    if (this.state.price){
+      price += this.state.price.toLocaleString(navigator.language, { minimumFractionDigits: 0 });
+    }
+
+
     return(
-      <div>
-         <img src={img}/>
+      <div className="car-review-page">
+        <h2 className="car-title">{this.state.year} {this.state.make} {this.state.model} </h2>
+         <img className="car-images "src={img}/>
+         <div className="price-mpg-0to60-flex">
+           <span className="price"> PRICE {price}</span>
+           <span className="mpg"> MPG {this.state.mpg}</span>
+           <span className="0to60"> 0-60 {this.state.acceleration}s</span>
+         </div>
           <p className="car-description">{this.state.description}</p>
       </div>
     );
