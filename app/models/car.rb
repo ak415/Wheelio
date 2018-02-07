e6a5c76ad97a3a1540e4692462596b4e05b3ab46
Akram <azaiezakrem@gmail.com>
2018-02-07 12:08:09 -0800

    ajax research calls working fine

--- app/models/car.rb
+++ app/models/car.rb
@@ -15,6 +15,22 @@ class Car < ApplicationRecord
 
 
     def self.search(query)
-      Car.where("(cars.make) ilike ?", "%#{query}%") 
-  end
+      new_query = query.split(" ")
+      results = []
+
+      new_query.each do |query|
+        year_results = (Car.where("(cars.year) = ?" , "#{query}".to_i))
+        make_results = Car.where("(cars.make) ilike ?", "%#{query}%")
+        model_results = (Car.where("(cars.model) ilike ?" , "%#{query}%"))
+
+        (make_results + year_results + model_results).each do |result|
+          results << result
+        end
+      end
+
+      results
+      # Car.where("(cars.make) ilike ?", "%#{query}%")
+      # .or(Car.where("(cars.model) ilike ?" , "%#{query}%"))
+        # .or(Car.where("CAST((cars.year) AS CHAR) = ?" , "%#{query}%"))
+    end
 end
