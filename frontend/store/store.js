2aef4be111ae180a8b2450646bf5c1131ec4be6d
Akram <azaiezakrem@gmail.com>
2018-03-14 13:37:46 -0700

    updated styling for car show page

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -4,11 +4,11 @@ import rootReducer from '../reducers/root_reducer';
 
 const middlewares = [thunk];
 
-if (process.env.NODE_ENV !== 'production') {
-  // must use 'require' (import only allowed at top of file)
-  const { logger } = require('redux-logger');
-  middlewares.push(logger);
-}
+// if (process.env.NODE_ENV !== 'production') {
+//   // must use 'require' (import only allowed at top of file)
+//   const { logger } = require('redux-logger');
+//   middlewares.push(logger);
+// }
 
 
 const configureStore = (preloadedState = {}) => (
